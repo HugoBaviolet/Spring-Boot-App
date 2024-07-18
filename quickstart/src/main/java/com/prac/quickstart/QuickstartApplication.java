727d097d4d0d50250aeac10f182a7e1976268634
Hugo A'Violet <hbaviolet@hotmail.co.uk>
2024-07-18 15:40:28 +0100

    test

--- quickstart/src/main/java/com/prac/quickstart/QuickstartApplication.java
+++ quickstart/src/main/java/com/prac/quickstart/QuickstartApplication.java
@@ -2,7 +2,7 @@ package com.prac.quickstart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//test comment
 @SpringBootApplication
 public class QuickstartApplication {
 
